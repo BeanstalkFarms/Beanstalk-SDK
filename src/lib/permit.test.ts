3411df3a8fd7feadc67d8db44ae1d81108aa4649
Silo Chad <silochad@protonmail.com>
2022-10-22 13:34:05 -0500

    add Root contract

--- src/lib/permit.test.ts
+++ src/lib/permit.test.ts
@@ -16,23 +16,23 @@ beforeAll(async () => {
   });
 })
 
-describe('ETH permit', () => {
-  it('can call permit on an ERC2612', async () => {
-    const sender = await sdk.getAccount();  // 0xf39...
-    const value = (1000*1E6).toString();    // 1000 BEAN
+// describe('ETH permit', () => {
+//   it('can call permit on an ERC2612', async () => {
+//     const sender = await sdk.getAccount();  // 0xf39...
+//     const value = (1000*1E6).toString();    // 1000 BEAN
 
-    const result = await sdk.permit.signERC2612(
-      sdk.tokens.BEAN.address,
-      sender,
-      spender,
-      value
-    );
+//     const result = await sdk.permit.signERC2612(
+//       sdk.tokens.BEAN.address,
+//       sender,
+//       spender,
+//       value
+//     );
 
-    const contract = ERC20Permit__factory.connect(sdk.tokens.BEAN.address, sdk.signer || sdk.provider);
+//     const contract = ERC20Permit__factory.connect(sdk.tokens.BEAN.address, sdk.signer || sdk.provider);
 
-    await contract.permit(sender, spender, value, result.deadline, result.split.v, result.split.r, result.split.s);
-    const allowance = await contract.allowance(sender, spender)
+//     await contract.permit(sender, spender, value, result.deadline, result.split.v, result.split.r, result.split.s);
+//     const allowance = await contract.allowance(sender, spender)
 
-    expect(allowance.toString()).to.equal(value);
-  });
-});
+//     expect(allowance.toString()).to.equal(value);
+//   });
+// });
