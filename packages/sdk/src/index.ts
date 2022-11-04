44713570df8019ecfbbdb0f844443a7002d41a98
Silo Chad <silochad@protonmail.com>
2022-11-04 16:56:17 -0500

    cleanup

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,11 +1,19 @@
-export * from "src/lib/BeanstalkSDK";
-export { ChainId } from "src/constants/chains";
-export { NativeToken, ERC20Token, BeanstalkToken, Token } from "src/classes/Token";
-export { TokenValue } from "src/classes/TokenValue";
-
+// Core
+export { BeanstalkSDK } from "src/lib/BeanstalkSDK";
 export * from "src/types";
 export * as Utils from "src/utils";
 
+// Constants
+export { ChainId } from "src/constants/chains";
+
+// Classes
+export { NativeToken, ERC20Token, BeanstalkToken, Token } from "src/classes/Token";
+export { TokenValue } from "src/classes/TokenValue";
+
+// Numbers
 export { DecimalBigNumber } from "src/utils/DecimalBigNumber";
-// export { FarmFromMode, FarmToMode } from "./lib/farm/types";
-export { FarmFromMode, FarmToMode } from "src/lib/farm/types";
+
+// Modules
+export * from "src/lib/farm/types";
+export type { TokenSiloBalance } from "src/lib/silo";
+export type { TokenBalance } from "src/lib/tokens";
