889887b1820564c67a2f8c77123d089d9eb6f65f
Silo Chad <silochad@protonmail.com>
2022-11-07 18:33:18 -0600

    add recursion test case

--- packages/sdk/src/lib/farm/work.test.ts
+++ packages/sdk/src/lib/farm/work.test.ts
@@ -2,8 +2,8 @@ import { ethers } from "ethers";
 import { setupConnection } from "../../utils.tests/provider";
 import { BeanstalkSDK } from "../BeanstalkSDK";
 
-let sdk : BeanstalkSDK;
-let account : string;
+let sdk: BeanstalkSDK;
+let account: string;
 
 beforeAll(async () => {
   const { provider, signer, account: _account } = await setupConnection();
@@ -11,20 +11,16 @@ beforeAll(async () => {
   sdk = new BeanstalkSDK({
     provider,
     signer,
-    subgraphUrl: 'https://graph.node.bean.money/subgraphs/name/beanstalk-testing'
+    subgraphUrl: "https://graph.node.bean.money/subgraphs/name/beanstalk-testing",
   });
-})
+});
 
-describe('Workflow', () => {
-  describe('add', () => {
-    it('handles a mixed array', async () => {
+describe("Workflow", () => {
+  describe("add", () => {
+    it("handles a mixed array", async () => {
       // Setup
       const farm = sdk.farm.create();
-      farm.add([
-        sdk.farm.presets.bean2usdt(),
-        async () => '0xEXAMPLE1',
-        async () => '0xEXAMPLE2',
-      ]);
+      farm.add([sdk.farm.presets.bean2usdt(), async () => "0xEXAMPLE1", async () => "0xEXAMPLE2"]);
 
       expect(farm.steps.length).toBe(3);
       expect(farm.stepResults.length).toBe(0);
@@ -33,15 +29,34 @@ describe('Workflow', () => {
       await farm.estimate(ethers.BigNumber.from(1000_000000));
 
       expect(farm.stepResults.length).toBe(3);
-      expect(farm.stepResults[1].encode(ethers.BigNumber.from(0)))
-        .toBe('0xEXAMPLE1')
-      expect(farm.stepResults[2].encode(ethers.BigNumber.from(0)))
-        .toBe('0xEXAMPLE2')
+      expect(farm.stepResults[1].encode(ethers.BigNumber.from(0))).toBe("0xEXAMPLE1");
+      expect(farm.stepResults[2].encode(ethers.BigNumber.from(0))).toBe("0xEXAMPLE2");
     });
 
-    it('throws on invalid input', async () => {
+    it("recurses through nested arrays of actions", async () => {
+      // Setup
       const farm = sdk.farm.create();
-      expect(() => farm.add({} as unknown as () => string)).toThrow('Unknown action type')
-    })
+      farm.add([
+        sdk.farm.presets.bean2usdt(),
+        async () => "0xEXAMPLE1",
+        [async () => "0xEXAMPLE2", async () => "0xEXAMPLE3", [async () => "0xEXAMPLE4"], async () => "0xEXAMPLE5"],
+      ]);
+
+      expect(farm.steps.length).toBe(6);
+      expect(farm.stepResults.length).toBe(0);
+
+      // Estimation
+      await farm.estimate(ethers.BigNumber.from(1000_000000));
+
+      expect(farm.stepResults.length).toBe(6);
+      expect(farm.stepResults[1].encode(ethers.BigNumber.from(0))).toBe("0xEXAMPLE1");
+      expect(farm.stepResults[2].encode(ethers.BigNumber.from(0))).toBe("0xEXAMPLE2");
+      expect(farm.stepResults[5].encode(ethers.BigNumber.from(0))).toBe("0xEXAMPLE5");
+    });
+
+    it("throws on invalid input", async () => {
+      const farm = sdk.farm.create();
+      expect(() => farm.add({} as unknown as () => string)).toThrow("Unknown action type");
+    });
   });
-})
+});
