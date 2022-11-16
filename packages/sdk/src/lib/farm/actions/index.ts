d3374abb19a257ab462560d8320420a455c18077
0xkokonut <0xkokonut@gmail.com>
2022-11-15 17:00:24 -0800

    silo withdrawal and claim

--- packages/sdk/src/lib/farm/actions/index.ts
+++ packages/sdk/src/lib/farm/actions/index.ts
@@ -6,5 +6,20 @@ import { AddLiquidity } from "./AddLiquidity";
 import { RemoveLiquidityOneToken } from "./RemoveLiquidityOneToken";
 import { TransferToken } from "./TransferToken";
 import { PermitERC20 } from "./PermitERC20";
+import { WithdrawDeposits } from "./WithdrawDeposits";
+import { ClaimWithdrawals } from "./ClaimWithdrawals";
+import { AttachPermitERC20 } from "./AttachPermitERC20";
 
-export { Exchange, ExchangeUnderlying, WrapEth, UnwrapEth, AddLiquidity, RemoveLiquidityOneToken, TransferToken, PermitERC20 };
+export {
+  Exchange,
+  ExchangeUnderlying,
+  WrapEth,
+  UnwrapEth,
+  AddLiquidity,
+  RemoveLiquidityOneToken,
+  TransferToken,
+  PermitERC20,
+  WithdrawDeposits,
+  ClaimWithdrawals,
+  AttachPermitERC20
+};
