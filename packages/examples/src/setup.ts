102fa1162441874015987e682ee8a06bd62ee833
ALECKS <0xalecks@gmail.com>
2022-11-22 12:31:46 -0500

    examples: better impersonation support

--- packages/examples/src/setup.ts
+++ packages/examples/src/setup.ts
@@ -1,4 +1,5 @@
 import { BeanstalkSDK, DataSource, TestUtils } from "@beanstalk/sdk";
+import { Provider } from "@beanstalk/sdk/dist/types/lib/BeanstalkSDK";
 import { ethers } from "ethers";
 
 export const provider = new ethers.providers.StaticJsonRpcProvider("http://localhost:8545");
@@ -6,10 +7,25 @@ export const { signer, account } = TestUtils.setupConnection(provider);
 
 export const sdk = new BeanstalkSDK({
   provider,
-  signer: signer,
+  // signer: signer,
   subgraphUrl: "https://graph.node.bean.money/subgraphs/name/beanstalk-testing",
   source: DataSource.LEDGER,
   DEBUG: true
 });
+
+export const impersonate = async (account) => {
+  const stop = await chain.impersonate(account);
+
+  const provider = ethers.getDefaultProvider("http://localhost:8545") as Provider;
+  const signer = await provider.getSigner(account);
+  const sdk = new BeanstalkSDK({
+    signer,
+    subgraphUrl: "https://graph.node.bean.money/subgraphs/name/beanstalk-testing",
+    source: DataSource.LEDGER,
+    DEBUG: true
+  });
+
+  return { sdk, stop };
+};
 
 export const chain = new TestUtils.BlockchainUtils(sdk);
