064577c79e98fda8ddf3f181cdf1b33229d45ee8
ALECKS <0xalecks@gmail.com>
2022-12-05 21:16:32 -0500

    sdk: add getTokenIndex to Pool class

--- packages/sdk/src/classes/Pool/CurveMetaPool.ts
+++ packages/sdk/src/classes/Pool/CurveMetaPool.ts
@@ -1,8 +1,21 @@
 import { CurveMetaPool__factory } from "src/constants/generated";
 import { TokenValue } from "src/TokenValue";
+import { ERC20Token, Token } from "../Token";
 import Pool, { Reserves } from "./Pool";
 
 export class CurveMetaPool extends Pool {
+  // This returns the index of the input token depending if
+  // it's an underlying token or not.
+  // Index shapes are as follows:
+  // [ BEAN, 3CRV ]
+  // [ DAI, USDC, USDT ] for underlying
+  public getTokenIndex(token: Token) {
+    let i = this.tokens.indexOf(token as ERC20Token);
+    if (i >= 0) return i;
+    // not found in main tokens, check underlying
+    return this.underlying.indexOf(token as ERC20Token);
+  }
+
   public getContract() {
     return CurveMetaPool__factory.connect(this.address, Pool.sdk.providerOrSigner);
   }
