9f23fa09ec3e80816a03c14702ac3eafc90aaa26
ALECKS <0xalecks@gmail.com>
2022-12-06 16:28:18 -0500

    sdk: Silo Deposit: add summary

--- packages/sdk/src/lib/silo/DepositOperation.ts
+++ packages/sdk/src/lib/silo/DepositOperation.ts
@@ -1,6 +1,7 @@
 import { ContractTransaction } from "ethers";
 import { Router } from "src/classes/Router";
 import { Token } from "src/classes/Token";
+import { ActionType } from "src/constants/actions";
 import { TokenValue } from "src/TokenValue";
 import { BeanstalkSDK } from "../BeanstalkSDK";
 import { FarmFromMode, FarmToMode, FarmWorkflow } from "../farm";
@@ -14,6 +15,7 @@ export class DepositOperation {
   inputAmount: TokenValue;
   readonly router: Router;
   workflow: FarmWorkflow<{ slippage: number } & Record<string, any>>;
+  lastAmountIn: TokenValue;
 
   constructor(sdk: BeanstalkSDK, router: Router, targetToken: Token, account: string) {
     if (!sdk.tokens.siloWhitelist.has(targetToken)) throw new Error(`Cannot deposit ${targetToken.symbol}, not on whitelist.`);
@@ -62,11 +64,55 @@ export class DepositOperation {
     console.log(this.router.getGraphCode());
   }
 
-  async estimate(amount: TokenValue) {}
+  async getSummary() {
+    const summary = [];
+    const fromToken = this.inputToken;
+    const toToken = this.targetToken;
+    const steps = this.workflow.summarizeSteps();
+    const depositStep = steps.find((s) => s.name === "deposit");
+    if (!depositStep) throw new Error("Deposit step not found");
+
+    // If there's more than one step, that means we swaped (or added liquidity which
+    // we count as a swap here)
+    if (steps.length > 1) {
+      summary.push({
+        type: ActionType.SWAP,
+        tokenIn: fromToken,
+        tokenOut: toToken,
+        amountIn: this.lastAmountIn,
+        amountOut: toToken.fromBlockchain(depositStep.amountOut)
+      });
+    }
+
+    const depositBDV = await DepositOperation.sdk.bean.getBDV(toToken, toToken.fromBlockchain(depositStep.amountOut));
+
+    summary.push({
+      type: ActionType.DEPOSIT,
+      amount: toToken.fromBlockchain(depositStep.amountOut),
+      token: toToken
+    });
+    summary.push({
+      type: ActionType.UPDATE_SILO_REWARDS,
+      stalk: toToken.getStalk(depositBDV),
+      seeds: toToken.getSeeds(depositBDV)
+    });
+
+    return summary;
+  }
+
+  async estimate(amountIn: TokenValue): Promise<TokenValue> {
+    if (this.workflow.length === 0) throw new Error("No available route in workflow");
+
+    this.lastAmountIn = amountIn;
+    const est = await this.workflow.estimate(amountIn);
+
+    return this.targetToken.fromBlockchain(est);
+  }
 
   async execute(amountIn: TokenValue, slippage: number): Promise<ContractTransaction> {
     if (this.workflow.length === 0) throw new Error("No available route in workflow");
 
+    this.lastAmountIn = amountIn;
     return this.workflow.execute(amountIn, { slippage });
   }
 }
