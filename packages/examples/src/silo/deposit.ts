dc8a9336d86b4060e3f6f3f8f5c522f92b6f650a
ALECKS <0xalecks@gmail.com>
2022-12-05 21:17:10 -0500

    sdk: deposits work with addLiquidity

--- packages/examples/src/silo/deposit.ts
+++ packages/examples/src/silo/deposit.ts
@@ -1,5 +1,5 @@
+import { BeanstalkSDK, ERC20Token, Token, TokenValue } from "@beanstalk/sdk";
 import chalk from "chalk";
-
 import { table } from "table";
 
 import { account as _account, impersonate } from "../setup";
@@ -15,17 +15,27 @@ async function main() {
   // Some of the claiming contract methods don't accept an (account) parameter
   // and work off of msg.sender, so we need to impersonate the passed account.
   const { sdk, stop } = await impersonate(account);
+  // sdk.DEBUG = false;
 
-  const target = sdk.tokens.BEAN_CRV3_LP;
-  const input = sdk.tokens.BEAN_CRV3_LP;
-  const amount = input.amount(400);
+  // await deposit(sdk.tokens.CRV3, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
+  // await deposit(sdk.tokens.BEAN, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
+  // await deposit(sdk.tokens.DAI, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
+  // await deposit(sdk.tokens.USDC, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
+  await deposit(sdk.tokens.USDT, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
+
+  await stop();
+}
+
+async function deposit(input: ERC20Token, target: Token, _amount: number, account: string, sdk: BeanstalkSDK) {
+  console.log(`Depositing ${_amount} ${input.symbol} to ${target.symbol} silo`);
+  const amount = input.amount(_amount);
   await input.approveBeanstalk(amount);
 
   const deposit = await sdk.silo.buildDeposit(target, account);
+  deposit.getGraph();
   deposit.setInputToken(input);
 
   const tx = await deposit.execute(amount, 0.1);
   await tx.wait();
-  console.log("done");
-  await stop();
+  console.log("DONE");
 }
