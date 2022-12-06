434c0e49fffbbf05068276f88f50d15005024d27
ALECKS <0xalecks@gmail.com>
2022-12-06 16:28:18 -0500

    sdk: Silo Deposit: add summary

--- packages/examples/src/silo/deposit.ts
+++ packages/examples/src/silo/deposit.ts
@@ -15,14 +15,15 @@ async function main() {
   // Some of the claiming contract methods don't accept an (account) parameter
   // and work off of msg.sender, so we need to impersonate the passed account.
   const { sdk, stop } = await impersonate(account);
-  // sdk.DEBUG = false;
+  sdk.DEBUG = false;
 
+  // await deposit(sdk.tokens.BEAN_CRV3_LP, sdk.tokens.BEAN_CRV3_LP, 500, account, sdk);
   // await deposit(sdk.tokens.CRV3, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
   // await deposit(sdk.tokens.BEAN, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
   // await deposit(sdk.tokens.DAI, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
   // await deposit(sdk.tokens.USDC, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
   // await deposit(sdk.tokens.USDT, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
-  await deposit(sdk.tokens.ETH, sdk.tokens.BEAN_CRV3_LP, 400, account, sdk);
+  await deposit(sdk.tokens.ETH, sdk.tokens.BEAN_CRV3_LP, 3, account, sdk);
 
   await stop();
 }
@@ -35,7 +36,15 @@ async function deposit(input: Token, target: Token, _amount: number, account: st
   const deposit = await sdk.silo.buildDeposit(target, account);
   deposit.setInputToken(input);
 
-  const tx = await deposit.execute(amount, 0.1);
-  await tx.wait();
+  const est = await deposit.estimate(amount);
+  console.log("Estimate:", est.toHuman());
+
+  const txr = await deposit.execute(amount, 0.1);
+  await txr.wait();
+
+  // // Show summary of actions
+  // for (const s of await deposit.getSummary()) {
+  //   console.log(s);
+  // }
   console.log("DONE");
 }
