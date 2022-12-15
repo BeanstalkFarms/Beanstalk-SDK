a20defaf2511825bcac85266dcee07a9b6ac8c71
ALECKS <0xalecks@gmail.com>
2022-12-15 16:32:23 -0500

    fix local

--- packages/examples/src/setup.ts
+++ packages/examples/src/setup.ts
@@ -2,7 +2,7 @@ import { BeanstalkSDK, DataSource, TestUtils } from "@beanstalk/sdk";
 import { Provider } from "@beanstalk/sdk/dist/types/lib/BeanstalkSDK";
 import { ethers } from "ethers";
 
-export const provider = new ethers.providers.StaticJsonRpcProvider("http://localhost:8545");
+export const provider = new ethers.providers.StaticJsonRpcProvider("http://127.0.0.1:8545");
 export const { signer, account } = TestUtils.setupConnection(provider);
 
 export const sdk = new BeanstalkSDK({
